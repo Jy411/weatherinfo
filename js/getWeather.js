f82323aca8c7a63fd428fbc5ac57103e9810ad47
Leon <kldotwon9@gmail.com>
2021-04-28 14:12:51 +0800

    updated get & set

--- js/getWeather.js
+++ js/getWeather.js
@@ -4,7 +4,6 @@ let apiKey = "6d46a8768e16465aa86a2f3f7ed580ea";
 let ipAddressUrl = "https://ipgeolocation.abstractapi.com/v1/?api_key=900470f4ca304fc09b194c1241f1f229";
 
 let currentUrl = "";
-let forecastUrl = "";
 
 // default unit
 let tempUnit = "M";
@@ -16,21 +15,17 @@ let searchBtn = document.getElementById("searchBtn");
 searchBtn.addEventListener("click", function(){
 	let searchInput = document.getElementById("searchInput").value;
 
-	let searchUrl = `https://api.weatherbit.io/v2.0/current?city=${searchInput}&units=${tempUnit}&key=${apiKey}`;
-	let searchUrl2 = `http://api.weatherbit.io/v2.0/forecast/daily?city=${searchInput}&units=${tempUnit}&days=7&key=${apiKey}`;
+	let searchUrl = `http://api.weatherbit.io/v2.0/forecast/daily?city=${searchInput}&units=${tempUnit}&days=7&key=${apiKey}`;
 	
 	getWeather(searchUrl);
-	getForecast(searchUrl2);
 });
 
 let currentLoc = document.getElementById("currentLoc");
 currentLoc.addEventListener("click", function(){
 	const data = getObject(ipAddressUrl);
-	currentUrl = `https://api.weatherbit.io/v2.0/current?lat=${data.latitude}&lon=${data.longitude}&units=${tempUnit}&key=${apiKey}`;
-	forecastUrl = `http://api.weatherbit.io/v2.0/forecast/daily?lat=${data.latitude}&lon=${data.longitude}&units=${tempUnit}&days=7&key=${apiKey}`;
+	currentUrl = `http://api.weatherbit.io/v2.0/forecast/daily?lat=${data.latitude}&lon=${data.longitude}&units=${tempUnit}&days=7&key=${apiKey}`;
 
 	getWeather(currentUrl);
-	getForecast(forecastUrl);
 });
 
 let changeUnit = document.getElementById("changeUnit");
@@ -48,8 +43,7 @@ changeUnit.addEventListener("click", function(){
 
 function getLatLon() {
 	const data = getObject(ipAddressUrl);
-	currentUrl = `https://api.weatherbit.io/v2.0/current?lat=${data.latitude}&lon=${data.longitude}&units=${tempUnit}&key=${apiKey}`;
-	forecastUrl = `http://api.weatherbit.io/v2.0/forecast/daily?lat=${data.latitude}&lon=${data.longitude}&units=${tempUnit}&days=7&key=${apiKey}`;
+	currentUrl = `http://api.weatherbit.io/v2.0/forecast/daily?lat=${data.latitude}&lon=${data.longitude}&units=${tempUnit}&days=7&key=${apiKey}`;
 }
 getLatLon();
 
@@ -61,7 +55,7 @@ getWeather(currentUrl);
 
 function setWeather(data) {
 	let weatherSrc = `https://www.weatherbit.io/static/img/icons/${data.data[0].weather.icon}.png`;
-	document.getElementById("city").innerText = data.data[0].city_name;
+	document.getElementById("city").innerText = data.city_name;
 	document.getElementById("temp").innerText = Math.round(data.data[0].temp);
 	document.getElementById("desc").innerText = data.data[0].weather.description;
 	document.getElementById("weatherIcon").setAttribute("src", weatherSrc);
@@ -71,6 +65,17 @@ function setWeather(data) {
 	//document.getElementById("uvLevel").innerText = Math.round(data.data[0].uv);
 	document.getElementById("airQuality").innerText = data.data[0].aqi;
 	setWeatherUnit(data);
+	let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+	for (let i = 0; i < 6; i++) {
+		let date = new Date(data.data[i+1].datetime);
+		let dd = date.getDate();
+		let mm = date.getMonth() + 1;
+		let day = days[date.getDay()];
+		document.getElementsByClassName("date")[i].innerText = dd + '/' + mm;
+		document.getElementsByClassName("dayTitle")[i].innerText = day;
+		document.getElementsByClassName("imgFc")[i].setAttribute("src", `https://www.weatherbit.io/static/img/icons/${data.data[i+1].weather.icon}.png`);
+		document.getElementsByClassName("tempForecast")[i].innerText = Math.round(data.data[i+1].temp);
+	}
 }
 
 function setWeatherUnit(data) {
@@ -90,22 +95,6 @@ function setWeatherUnit(data) {
 		//document.getElementById("visibility").innerText = data.data[0].vis + " m";
 	}
 }
-
-function getForecast(url) {
-	const data = getObject(url);
-	let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-	for (let i = 0; i < 6; i++) {
-		let date = new Date(data.data[i+1].datetime);
-		let dd = date.getDate();
-		let mm = date.getMonth() + 1;
-		let day = days[date.getDay()];
-		document.getElementsByClassName("date")[i].innerText = dd + '/' + mm;
-		document.getElementsByClassName("dayTitle")[i].innerText = day;
-		document.getElementsByClassName("imgFc")[i].setAttribute("src", `https://www.weatherbit.io/static/img/icons/${data.data[i+1].weather.icon}.png`);
-		document.getElementsByClassName("tempForecast")[i].innerText = Math.round(data.data[i+1].temp);
-	}
-}
-getForecast(forecastUrl);
 
 function getObject(url){
 	let HttpRequest = new XMLHttpRequest();
