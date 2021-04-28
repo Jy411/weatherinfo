bd4febab310edff0d6fb061b4507c8c94d784733
jy411 <jinyungisnot@gmail.com>
2021-04-28 13:53:44 +0800

    testing search suggestion js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// fetch auto suggested locations
+const locationSearchInputEl = document.getElementById("locationSearch");
+
+const API_KEY = "AIzaSyBJpM_WCkXC5L-2X1xBA6D4wpaHvJVS1fw";
+const GOOGLE_API_URL = "https://maps.googleapis.com/maps/api/place/autocomplete/json?" + API_KEY;
+const getLocationSuggestions = (locationInput) => {
+  fetch(GOOGLE_API_URL + "&location=" + locationInput)
+      .then(response => response.json())
+      .then(data => console.log(data));
+}
+
+locationSearchInputEl.oninput = (data) => getLocationSuggestions(data.target.value);
