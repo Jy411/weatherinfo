255484e36aa49d8af70e9ff09e872a8c190d1bfd
jy411 <jinyungisnot@gmail.com>
2021-05-04 21:14:22 +0800

    fixed url v2

--- js/searchSuggestions.js
+++ js/searchSuggestions.js
@@ -2,6 +2,9 @@ function initSearch() {
   const input = document.getElementById("locationSearch");
   const searchBtn = document.getElementById("searchBtn");
   const autocomplete = new google.maps.places.Autocomplete(input);
+  autocomplete.setOptions({
+    types: ["cities"],
+  })
   input.addEventListener('keypress', (ev) => {
     if (ev.key === 'Enter') {
       searchBtn.click();
