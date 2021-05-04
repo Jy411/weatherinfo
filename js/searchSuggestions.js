fb290aad97fba44efa91734750b7f8eff4c37788
jy411 <jinyungisnot@gmail.com>
2021-05-04 21:21:22 +0800

    search granularity fixed for reals this time

--- js/searchSuggestions.js
+++ js/searchSuggestions.js
@@ -1,10 +1,10 @@
 function initSearch() {
   const input = document.getElementById("locationSearch");
   const searchBtn = document.getElementById("searchBtn");
-  const autocomplete = new google.maps.places.Autocomplete(input);
-  autocomplete.setOptions({
-    types: ["cities"],
-  })
+  const options = {
+    types: ["(cities)"]
+  };
+  const autocomplete = new google.maps.places.Autocomplete(input, options);
   input.addEventListener('keypress', (ev) => {
     if (ev.key === 'Enter') {
       searchBtn.click();
